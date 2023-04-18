17fa0a5d3e1035d57812785771e4d124976c7265
Caleb <caleb.phillips@nrel.gov>
2023-04-18 10:24:52 -0400

    Update for 2022

--- plot.R
+++ plot.R
@@ -3,6 +3,6 @@ d <- read.csv('pubs.csv',header=T)
 library(ggplot2)
 
 qplot(factor(year),data=d,geom="bar",fill=factor(cat),xlab="Year",ylab="Count") + 
-  scale_fill_brewer(name="Area",palette="Set3") + ggtitle("") + 
+  scale_fill_brewer(name="Subject Area",palette="Set3") + ggtitle("") + 
   theme(plot.background=element_rect(fill = "transparent",colour = NA))
 ggsave("pubs.jpg",width=10,height=3)
